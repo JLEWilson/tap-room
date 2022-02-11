e4bf2082e243940beeccd7024d31f411e38cccb3
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-11 13:16:50 -0900

    add restock button

---
+++ src/components/KegDisplay/SelectedKegPane/ReStockButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { v4 } from 'uuid';
+
+function RestockButton(props) {
+  const RestockButtonStyles = {
+    width: "5em"
+  }
+  const centerButton = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+
+  function handleAddKegFormSubmission(event) {
+    event.preventDefault();
+    const kegToReorder = {
+      id: v4(),
+          name: props.selectedKeg.name,
+          brand: props.selectedKeg.brand,
+          price:props.selectedKeg.price,
+          abv: props.selectedKeg.abv,
+          pints: 124
+    }
+    props.newKeg(kegToReorder);
+  }
+
+  return(
+    <React.Fragment>
+      <form onSubmit={handleAddKegFormSubmission}>
+        <div style={centerButton}>
+          <Button variant="dark" >Restock This Keg</Button>
+        </div>
+      </form>
+    </React.Fragment>
+  )
+}
+
+export default RestockButton;
