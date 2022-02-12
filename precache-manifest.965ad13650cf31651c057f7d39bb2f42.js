14d505095bb4b74ad6583abbfe7742906d627231
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-11 15:36:52 -0900

    Updates

---
+++ precache-manifest.965ad13650cf31651c057f7d39bb2f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97310d8914e7b32de7ca0ee2e3979fc",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "20af6971145a989d30a0",
+    "url": "/tap-room/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "7eb99fb62768f124b04d",
+    "url": "/tap-room/static/css/main.10db8df8.chunk.css"
+  },
+  {
+    "revision": "20af6971145a989d30a0",
+    "url": "/tap-room/static/js/2.51aca34b.chunk.js"
+  },
+  {
+    "revision": "81adc2e1c469174d9813",
+    "url": "/tap-room/static/js/3.b9962ed3.chunk.js"
+  },
+  {
+    "revision": "7eb99fb62768f124b04d",
+    "url": "/tap-room/static/js/main.9ae61a4e.chunk.js"
+  },
+  {
+    "revision": "948703c68cd899727821",
+    "url": "/tap-room/static/js/runtime-main.6c3d4911.js"
+  }
+]);
